d22130c51b6fa9d43b08ee896b04aba88acbf7de
Jesse Charbneau <jessecharbneau@gmail.com>
2024-03-30 19:37:46 -0400

    updates to support schema's for market and gdp values

--- dataset/init.sql
+++ dataset/init.sql
@@ -1,19 +1,27 @@
 
 
-/* create users table */
-CREATE TABLE IF NOT EXISTS users (
-    user_id serial PRIMARY KEY,
-    username VARCHAR (50) UNIQUE NOT NULL,
-    password VARCHAR (50) NOT NULL,
-    email VARCHAR (255) UNIQUE NOT NULL,
-    created_on TIMESTAMP NOT NULL,
-    last_login TIMESTAMP
+/* create our schema */
+
+
+/* create a table to track the overall stock market value */
+create table if not exists stock_market_value (
+        id serial PRIMARY KEY,
+        source VARCHAR (50),
+        market_value NUMERIC(15,2),
+        last_updated TIMESTAMP,
+        created_on TIMESTAMP NOT NULL
 );
 
+/* create our schema to track the gdp value over time */
+create table if not exists gdp_value (
+    id serial PRIMARY KEY,
+    source VARCHAR (50),
+    gdp_value NUMERIC(15,2),
+    last_updated TIMESTAMP,
+    created_on TIMESTAMP NOT NULL
+);
 
-/* create results table(s) */
-/*CREATE TABLE IF NOT EXISTS survey_results (
-    survey_id serial PRIMARY_KEY,
+/* load our most recent pull */
 
 
- */
+
