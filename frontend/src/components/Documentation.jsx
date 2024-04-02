36d0e60826bbee42f66a4a0eef17184e8e7cdc8f
Jesse Charbneau <jessecharbneau@gmail.com>
2024-04-01 22:35:43 -0400

    stubbing out the documentation section

---
+++ frontend/src/components/Documentation.jsx
@@ -0,0 +1,14 @@
+import { SectionWrapper } from '../hoc'
+import React from "react";
+const Documentation = () => {
+    return (
+        <>
+            <div className="w-full max-w-7xl h-full pt-20">
+               documentation
+            </div>
+        </>
+    )
+}
+
+
+export default SectionWrapper (Documentation, "docs")
