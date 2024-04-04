ed8de1c4f9fe60b9281a00da1544f197ac0a655a
Jesse Charbneau <jessecharbneau@gmail.com>
2024-04-04 08:25:19 -0400

    first draft of api integration utility

---
+++ frontend/src/utils/api.js
@@ -0,0 +1,19 @@
+// api.js
+const API_URL = 'http://localhost:8000';
+    // process.env.REACT_APP_API_URL;
+
+export const fetchMarketInsights = async () => {
+    const response = await fetch(`${API_URL}/v1/market_insights`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch market insights');
+    }
+    return await response.json();
+}
+
+export const fetchMarketTrends = async () => {
+    const response = await fetch(`${API_URL}/v1/market_trends`);
+    if (!response.ok) {
+        throw new Error('Failed to fetch market trends');
+    }
+    return await response.json();
+}
