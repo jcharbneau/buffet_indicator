09a58681002354ce86212b9de5cb1dded92ab62e
Jesse Charbneau <jessecharbneau@gmail.com>
2024-04-04 08:20:42 -0400

    adding in several files - for api, adding in database, models, and schemas to support pydantic and sqlalchemy - for initial pipeline development, added two files process_data and process_data2 (testing currently) - additionally, stubbed out a postgresql.conf file (not yet in use)

---
+++ api/database.py
@@ -0,0 +1,32 @@
+from databases import Database
+from sqlalchemy import create_engine, MetaData
+from sqlalchemy.ext.declarative import declarative_base
+from sqlalchemy.orm import sessionmaker
+import os
+from dotenv import load_dotenv
+
+# Load environment variables from .env file
+
+# Database connection details
+DATABASE = 'postgres'
+USER = 'pipeline_user'
+PASSWORD = 'pipeline_password'
+HOST = 'localhost'
+PORT = '5432'
+DATABASE_URL = f"postgresql://{USER}:{PASSWORD}@{HOST}:{PORT}/{DATABASE}"
+
+# Database connection settings
+# DATABASE_URL = os.getenv("DATABASE_URL")  # Example: "postgresql://user:password@localhost/dbname"
+
+# SQLAlchemy setup for synchronous operations (like migrations)
+engine = create_engine(DATABASE_URL)
+SessionLocal = sessionmaker(autocommit=False, autoflush=False, bind=engine)
+
+# Metadata instance for schema generation
+metadata = MetaData()
+
+# Base class for declarative models
+Base = declarative_base(metadata=metadata)
+
+# databases package setup for async operations
+database = Database(DATABASE_URL)
